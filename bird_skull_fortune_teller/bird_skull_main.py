e32cde2aaeeb68b99241e4d2aea9e1496c6de901
khn <keith.nelson@gmail.com>
2020-08-29 00:01:29 -0500

    Some basic script commands

--- bird_skull_fortune_teller/bird_skull_main.py
+++ bird_skull_fortune_teller/bird_skull_main.py
@@ -1,6 +1,12 @@
 import maestro
 import time
+import keyboard
 
 servo = maestro.Controller()
-time.sleep(3)
+servo.runScriptSub(1)
+time.sleep(.5)
+servo.runScriptSub(2)
+time.sleep(1.5)
+#servo.setAccel(2,0)
 servo.runScriptSub(0)
+servo.close
