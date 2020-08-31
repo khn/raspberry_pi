3d208243fc5aecc3433617bf9f0f71559769d34d
khn <keith.nelson@gmail.com>
2020-08-30 22:54:15 -0500

    Working with keyboard. (Clunky, but it works)

--- bird_skull_fortune_teller/bird_skull_main.py
+++ bird_skull_fortune_teller/bird_skull_main.py
@@ -2,4 +2,17 @@ import fortun_teller
 
 bird = fortun_teller.Head()
 
-bird.move("right", "fast")
+
+while True:
+    x = input('Where would you like me to go? ')
+    if x == "r":
+        bird.move("right", "fast")
+    if x == "l":
+        bird.move("left", "fast")
+    if x == "u":
+        bird.move("up", "fast")
+    if x == "d":
+        bird.move("down","fast")
+    if x == "q":
+        bird.close()
+        break
--- bird_skull_fortune_teller/fortun_teller.py
+++ bird_skull_fortune_teller/fortun_teller.py
@@ -1,18 +1,19 @@
 import maestro
+import time
 
 # These are the motor controller limits, measured in Maestro units.
 # These default values typically work fine and align with maestro's default limits.
 # Vaules should be adjusted so that center stops the motors and the min/max values
 # limit speed range you want for your robot.
-MIN_L = 4000
-MIN_R = 4000
-MIN_C = 4000
-CENTER_L = 6000
-CENTER_R = 6000
-CENTER_C = 6000
-MAX_L = 8000
-MAX_R = 8000
-MAX_C = 8000
+MIN_L = 992
+MIN_R = 992
+MIN_C = 992
+CENTER_L = 1500
+CENTER_R = 1500
+CENTER_C = 1500
+MAX_L = 1900
+MAX_R = 1900
+MAX_C = 1900
 # Sets what pins each motor is connected to on the Maestro board
 CH_L = 0
 CH_R = 1
@@ -26,13 +27,13 @@ class Head:
         # Init motor accel/speed params
         # Left side (when looking face on)
         self.head.setAccel(CH_L, 0)
-        self.head.setSpeed(CH_L, 60)
+        self.head.setSpeed(CH_L, 0)
         # Right side (when looking face on)
         self.head.setAccel(CH_R, 0)
-        self.head.setSpeed(CH_R, 60)
+        self.head.setSpeed(CH_R, 0)
         # Center or head rotation motor
         self.head.setAccel(CH_C, 0)
-        self.head.setSpeed(CH_C, 60)
+        self.head.setSpeed(CH_C, 0)
         # Motor min/center/max values
         self.minL = MIN_L
         self.minR = MIN_R
@@ -42,22 +43,51 @@ class Head:
         self.maxR = MAX_R
 
     def _move_head(self, where, speed):
+        print("Well, here we are")
         if where == "right":
-            self.head.runScriptSub(1)
+            self.head.setSpeed(0, 60)  # set speed of servo 1
+            self.head.setSpeed(1, 60)
+            self.head.setSpeed(2, 30)
+            # self.head.runScriptSub(0)
+            self.head.setTarget(0, 5488 )  # set servo to move to center position
+            self.head.setTarget(1, 5934 )  # set servo to move to center position
+            self.head.setTarget(2, 5856 )  # set servo to move to center position
+            time.sleep(.3)
         elif where == "left":
-            self.head.setAccel(0, 4)  # set servo 0 acceleration to 4
-            self.head.setTarget(0, 6000)  # set servo to move to center position
-            self.head.setSpeed(1, 10)  # set speed of servo 1
-            x = self.head.getPosition(1)  # get the current position of servo 1
-            self.head.close()
-        pass
+            self.head.setAccel(0, 200)  # set servo 0 acceleration to 4
+            self.head.setAccel(1, 200)  # set servo 0 acceleration to 4
+            self.head.setAccel(2, 180)  # set servo 0 acceleration to 4
+            self.head.setSpeed(0, 0)  # set speed of servo 1
+            self.head.setSpeed(1, 0)
+            self.head.setSpeed(2, 0)
+            self.head.setTarget(0, 5190 )  # set servo to move to center position
+            self.head.setTarget(1, 6843 )  # set servo to move to center position
+            self.head.setTarget(2, 7697 )  # set servo to move to center position
+            time.sleep(.3)
+            self.head.setTarget(0, 7553 )  # set servo to move to center position
+            self.head.setTarget(1, 5868 )  # set servo to move to center position
+            # self.head.setTarget(2, 7697 )  # set servo to move to center position
+            # self.head.setSpeed(1, 10)  # set speed of servo 1
+            time.sleep(.7)
+            self.head.setTarget(0, 5190 )  # set servo to move to center position
+            self.head.setTarget(1, 6843 )  # set servo to move to center position
+            # self.head.setTarget(2, 7697 )  # set servo to move to center position
+            time.sleep(.3)
+            self.head.setSpeed(0, 170)  # set speed of servo 1
+            self.head.setSpeed(1, 170)
+            self.head.setTarget(0, 5488 )  # set servo to move to center position
+            self.head.setTarget(1, 5934 )  # set servo to move to center position
+            # self.head.setTarget(2, 7697 )  # set servo to move to center position
+            time.sleep(.3)
 
     def move(self, look, speed):
         if look == "right":
-            self._move_head("right_side", speed)
+            self._move_head("right", speed)
+        elif look == "left":
+            self._move_head("left", speed)
 
-        pass
-
+    def close(self):
+        self.head.close()
 
 
 
